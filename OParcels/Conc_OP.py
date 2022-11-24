25f965cd56643068570aa3cee217e4c904a6cadb
jvalenti <jvalenti@eoas.ubc.ca>
2022-11-24 10:27:31 -0800

    concentration MF calculated from populations estimates

--- OParcels/Conc_OP.py
+++ OParcels/Conc_OP.py
@@ -19,6 +19,7 @@ def load_config(config_yaml):
    return config
 
 def get_conc(latmin,latmax,lonmin,lonmax, conc):
+    mask = xr.open_dataset(paths['mask'])
     z = mask.gdepw_0[0,:,240,340]
     vol=xr.open_dataset('/home/jvalenti/MOAD/grid/grid/mesh_maskBV201702.nc')['volume_cell']
     jjii = xr.open_dataset('~/MOAD/grid/grid/grid_from_lat_lon_mask999.nc')
@@ -48,13 +49,21 @@ def get_conc(latmin,latmax,lonmin,lonmax, conc):
         SD.append(np.std(valuess)/np.sqrt(Len))
     return Mean,SD
 def MP_measure(conc):
+    print('0% done')
     PugC_MP,PugC_SE=get_conc(47.65,47.75,-122.5,-122.3,conc)
+    print('15% done')
     PugN_MP,PugN_SE=get_conc(48,48.1,-122.75,-122.55,conc)
+    print('30% done')
     JdFE_MP,JdFE_SE=get_conc(48.2,48.3,-123.25,-123.05,conc)
+    print('45% done')
     JdFW_MP,JdFE_SW=get_conc(48.3,48.4,-124.25,-124.05,conc)
+    print('60% done')
     SoGC_MP,SoGC_SE =get_conc(49.3,49.4,-124,-123.8,conc)
+    print('75% done')
     SoGN_MP,SoGN_SE =get_conc(49.8,49.9,-124.9,-124.7,conc)
+    print('90% done')
     Fraser_MP,Fraser_SE =get_conc(49,49.1,-123.5,-123.3,conc)
+    print('100% done')
     return PugC_MP,PugC_SE,PugN_MP,PugN_SE,JdFE_MP,JdFE_SE,JdFW_MP,JdFE_SW,SoGC_MP,SoGC_SE,SoGN_MP,SoGN_SE,Fraser_MP,Fraser_SE
 
 
@@ -91,24 +100,25 @@ def Conc_OP(config):
     dsscon= float(MFc)
     dssdep=np.array(dss.z)
     
-    for i in range(len(dss)):
-        if i%5000==0:
-            print(f'{100*i/len(dss):.2f}% done.')
-        jj = jjii.jj.sel(lats=dssla[i], lons=dsslo[i], method='nearest').item()
-        ii = jjii.ii.sel(lats=dssla[i], lons=dsslo[i], method='nearest').item()
-        try:
-            dep = (np.abs(arr - dssdep[i])).argmin()
-            if arr[dep] > dssdep[i]:
-                dep+=-1
-            conc[jj,ii,dep] += dsscon
-        except ValueError:
-            pass
+    # for i in range(len(dss)):
+    #     if i%5000==0:
+    #         print(f'{100*i/len(dss):.2f}% done.')
+    #     jj = jjii.jj.sel(lats=dssla[i], lons=dsslo[i], method='nearest').item()
+    #     ii = jjii.ii.sel(lats=dssla[i], lons=dsslo[i], method='nearest').item()
+    #     try:
+    #         dep = (np.abs(arr - dssdep[i])).argmin()
+    #         if arr[dep] > dssdep[i]:
+    #             dep+=-1
+    #         conc[jj,ii,dep] += dsscon
+    #     except ValueError:
+    #         pass
         
-    data_set=xr.Dataset(coords={'lat': (['x', 'y'], coords.nav_lat.data),
-                    'lon': (['x', 'y'], coords.nav_lon.data),'depth':arr})
-    data_set["Prob"]=(['x', 'y','z'], conc)
-    param = load_config(config)
-    data_set.load().to_netcdf(path='/home/jvalenti/MOAD/results/'+param['file']['name']+'_prob'+str(param['startdate']['year'])+'.nc')
+    # data_set=xr.Dataset(coords={'lat': (['x', 'y'], coords.nav_lat.data),
+    #                 'lon': (['x', 'y'], coords.nav_lon.data),'depth':arr})
+    # data_set["Prob"]=(['x', 'y','z'], conc)
+    conc=xr.open_dataarray('/home/jvalenti/MOAD/results/Salish2018_long_prob2018.nc')
+    #param = load_config(config)
+    #data_set.load().to_netcdf(path='/home/jvalenti/MOAD/results/'+param['file']['name']+'_prob'+str(param['startdate']['year'])+'.nc')
     PugC_MP,PugC_SE,PugN_MP,PugN_SE,JdFE_MP,JdFE_SE,JdFW_MP,JdFE_SW,SoGC_MP,SoGC_SE,SoGN_MP,SoGN_SE,Fraser_MP,Fraser_SE=MP_measure(conc)
     dict = {'PugC_MP':PugC_MP,'PugC_SE':PugC_SE,'PugN_MP':PugN_MP,'PugN_SE':PugN_SE,'JdFE_MP':JdFE_MP,'JdFE_SE':JdFE_SE,'JdFW_MP':JdFW_MP,
     'JdFE_SW':JdFE_SW,'SoGC_MP':SoGC_MP,'SoGC_SE':SoGC_SE,'SoGN_MP':SoGN_MP,'SoGN_SE':SoGN_SE,'Fraser_MP':Fraser_MP,'Fraser_SE':Fraser_SE}
