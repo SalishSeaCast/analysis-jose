1b69bf21e049fc8a0e92bbcdea4b0fdee90e03a3
jvalenti <jvalenti@eoas.ubc.ca>
2023-03-12 22:34:24 -0700

    testing sinking inertial

---
+++ OParcels/fibres_OP23.py
@@ -0,0 +1,174 @@
+import sys
+import xarray as xr
+import numpy as np
+import os
+import yaml
+import math
+from datetime import datetime, timedelta
+from parcels import FieldSet, Field, VectorField, ParticleSet, JITParticle, ErrorCode, ParcelsRandom, Variable
+
+sys.path.append('/home/jvalenti/MOAD/analysis-jose/Source')
+from OP_functions23 import *
+
+def fibers_OP(config,local=0,restart=0):
+    param = load_config(config)
+    #Definitions
+    start = datetime(param['startdate']['year'], param['startdate']['month'], param['startdate']['day']) #Start date
+    length = param['param']['length'] # Set Time length [days] 
+    dt = param['param']['dt'] #toggle between - or + to pick backwards or forwards 
+    N = param['param']['N'] # number of deploying locations
+    n = param['param']['n'] # 1000   # number of particles per location
+    dmin = param['param']['dmin'] #minimum depth
+    dd = param['param']['dd'] #max depth difference from dmin
+    name = param['file']['name'] #name output file
+    dtp = param['param']['dtp'] #how often particle released in hours
+    odt = param['param']['odt'] #how often data is recorded
+    rrr = param['param']['r'] #radious of particle deployment
+    distr = param['param']['distr']
+    MFc = param['param']['MFc']
+# Define paths
+    paths = path(local)
+#Set outfall coordinates (Modify to choose other deploying location)    
+    #coord=xr.open_dataset(paths['coords'],decode_times=False)
+    clat = param['param']['lats']
+    clon = param['param']['lons']
+    #clon, clat = [float(outf_lon)],[float(outf_lat)] 
+    duration = timedelta(days=length)
+#Set deploy locations
+    if distr == 'hmg':
+        clat,clon = p_unidist(N,N)
+        N = len(clat)
+    elif distr == 'trst':
+        clat,clon = transect_deploy(clat,clon,N)
+    elif distr == 'std':
+        N = len(param['param']['lats'])
+    elif distr == 'pd':
+        clat, clon, N = pandas_deploy(N,MFc,int(dtp))
+        n = 1
+
+
+    x_offset, y_offset, z = p_deploy(N,n,dmin,dd,rrr)
+
+    lon = np.zeros([N,n])
+    lat = np.zeros([N,n])
+    for i in range(N):
+        lon[i,:]=(clon[i] + x_offset[i,:])
+        lat[i,:]=(clat[i] + y_offset[i,:])
+
+#Set start date time and the name of the output file
+
+    daterange = [start+timedelta(days=i) for i in range(length)]
+    fn =  name + '_'.join(d.strftime('%Y%m%d')+'_1n' for d in [start, start+duration]) + '.nc'
+    outfile = os.path.join(paths['out'], fn)
+####BUILD FIELDS FOR SIMULATION######
+    #Fill in the list of variables that you want to use as fields
+    varlist=['U','V','W']
+    filenames,variables=filename_set(start,length,varlist,local)
+    dimensions = {'lon': 'glamf', 'lat': 'gphif', 'depth': 'depthw','time': 'time_counter'}
+    field_set=FieldSet.from_nemo(filenames, variables, dimensions, allow_time_extrapolation=True)
+
+    #Find file names and variable names
+    varlist=['US','VS','WL','Diat','Flag','R','T','S','FS','ssh','Bathy','Kz','totdepth']
+    filenames,variables=filename_set(start,length,varlist,local)
+
+    #Add Stokes Drift fields
+    dimensions = {'lon': 'longitude', 'lat': 'latitude', 'time': 'time'}
+    us = Field.from_netcdf(filenames['US'], variables['US'], dimensions,allow_time_extrapolation=True)
+    vs = Field.from_netcdf(filenames['VS'], variables['VS'], dimensions,allow_time_extrapolation=True)
+    wl = Field.from_netcdf(filenames['WL'], variables['WL'], dimensions,allow_time_extrapolation=True)
+    field_set.add_field(us)
+    field_set.add_field(vs)
+    field_set.add_field(wl)
+    field_set.add_vector_field(VectorField("stokes", us, vs, wl))
+
+    #Add Vertical diffusivity coefficient field
+    dimensions = {'lon': 'glamt', 'lat': 'gphit', 'depth': 'depthw','time': 'time_counter'}
+    Kz = Field.from_netcdf(filenames['Kz'], variables['Kz'], dimensions,allow_time_extrapolation=True)
+    field_set.add_field(Kz)
+
+    #Add fields located at node T
+    dimensions = {'lon': 'glamt', 'lat': 'gphit', 'depth': 'deptht','time': 'time_counter'}
+    Diat = Field.from_netcdf(filenames['Diat'], variables['Diat'], dimensions,allow_time_extrapolation=True)
+    Flag = Field.from_netcdf(filenames['Flag'], variables['Flag'], dimensions,allow_time_extrapolation=True)
+    R = Field.from_netcdf(filenames['R'], variables['R'], dimensions,allow_time_extrapolation=True)
+    S = Field.from_netcdf(filenames['S'], variables['S'], dimensions,allow_time_extrapolation=True)
+    T = Field.from_netcdf(filenames['T'], variables['T'], dimensions,allow_time_extrapolation=True)
+    field_set.add_field(Diat)
+    field_set.add_field(Flag)
+    field_set.add_field(R)
+    field_set.add_field(S)
+    field_set.add_field(T)
+
+    #Add Bathymetry 2D field
+    dimensions = {'lon': 'glamt', 'lat': 'gphit'}
+    Bth = Field.from_netcdf(filenames['Bathy'], variables['Bathy'], dimensions,allow_time_extrapolation=True)
+    TD = Field.from_netcdf(filenames['totdepth'], variables['totdepth'], dimensions,allow_time_extrapolation=True)
+    field_set.add_field(Bth)
+    field_set.add_field(TD)
+
+    #Add SSH and Rivers 2D fields
+    dimensions = {'lon': 'glamt', 'lat': 'gphit','time': 'time_counter'}
+    cms = Field.from_netcdf(filenames['ssh'], variables['ssh'], dimensions,allow_time_extrapolation=True)
+    field_set.add_field(cms)
+    Fraser = Field.from_netcdf(filenames['FS'], variables['FS'], dimensions,allow_time_extrapolation=True,timestamps=get_timestamps(start,length))
+    field_set.add_field(Fraser)
+    
+    MPParticle = particle_maker(param)
+
+    
+######RUN OCEAN PARCELS WITH DEFINED PARTICLE AND PRESET FIELDS
+    if restart==1:
+        name_temp=find_temp(paths['out'])
+        os.system(f"cd {paths['out']} && parcels_convert_npydir_to_netcdf {name_temp}")
+        outfile=newest(paths['out'])
+        pset = ParticleSet.from_particlefile(field_set, MPParticle,outfile)
+    else:
+        if dtp == 0:
+            pset = ParticleSet.from_list(field_set, MPParticle, lon=lon, lat=lat, depth=z,time=start+timedelta(hours=odt))
+        else:
+            pset = ParticleSet.from_list(field_set, MPParticle, lon=lon, lat=lat, depth=z, repeatdt = timedelta(hours=dtp))
+    
+
+    KERNELS =  Buoyancy + pset.Kernel(Advection) + pset.Kernel(Stokes_driftRK3) + pset.Kernel(turb_mix) + pset.Kernel(Beaching) + pset.Kernel(Unbeaching)
+    
+    pset.execute(KERNELS,
+                runtime=duration, 
+                dt=dt,
+                output_file=pset.ParticleFile(name=outfile, outputdt=timedelta(hours=odt)),
+                recovery={ErrorCode.ErrorOutOfBounds: DeleteParticle})
+
+
+def find_temp(rootdir):
+    dirs=[]
+    for file in os.listdir(rootdir):
+        d = os.path.join(rootdir, file)
+        if os.path.isdir(d):
+            dirs.append(d)
+    temp=sorted(dirs, key=lambda x: os.path.getctime(x), reverse=True)[:1][0]
+    return temp[-12:]
+
+def newest(path):
+    files = os.listdir(path)
+    paths = [os.path.join(path, basename) for basename in files]
+    return max(paths, key=os.path.getctime)
+
+def get_timestamps(start,length):
+    timestamps=[]
+    duration = timedelta(days=length)
+    for day in range(duration.days):
+        timestamps.append([start + timedelta(days=day)])
+    return np.array(timestamps, dtype='datetime64')
+
+if __name__=="__main__":
+    try:
+        config,restart = sys.argv[1:]
+        config = [str(config)]
+    except ValueError:
+        pass
+        try:
+            config = sys.argv[1:]
+            restart=0
+        except :
+            print('Something went wrong')
+    fibers_OP(config,restart)
+     
---
+++ Source/OP_Kernels23.py
@@ -0,0 +1,204 @@
+def Buoyancy(particle, fieldset, time):
+    """Stokes law calculating settling velocity"""
+    if particle.beached == 0: #Check particle is in the water column    
+        if particle.tau==0: #Check age particle is 0     
+            particle.diameter = ParcelsRandom.normalvariate(particle.diameter, particle.SDD) #Randomly assign a value of diameter inside the Bamfield mesocosm size dist
+            particle.length = ParcelsRandom.normalvariate(particle.length, particle.SDL) #Same for length     
+        d = particle.diameter # particle diameter
+        l = particle.length # particle length
+        g = 9.8 #Gravity
+        #? rhob=1080 #HBac density fixed
+        #? Vcell=8.3e-19 #volume Hbac cell fixed bacilus avg
+        t = fieldset.votemper[time, particle.depth, particle.lat, particle.lon] #Loading temperature from SSC
+        ro = fieldset.sigma_theta[time, particle.depth, particle.lat, particle.lon] #Loading density sw from SSC
+        NN = particle.Nbac #Number of bacteria attached to MP
+        #? th= (Vcell*NN)/(5*math.pi*(d/2)*l+(d/2)**2) #rough approximation of thickness biofilm
+        #? rho=-1000-ro+particle.ro*l*(d/2)**2 + rhob*(1-((l*(d/2)**2)/(l*(d/2)**2 + 2*th*(d/2)**2 + l*th**2 + 2*th**3))) #Total density considering biofilm (- sw density) )
+        #? d+=2*th #diameter considering biofilm
+        #? l+=2*th #length considering biofilm
+        visc = 4.2844e-5 + 1/(0.157*((t + 64.993)**2)-91.296) #kinematic viscosity for Temp of SSC
+        Ws= ((l/d)**-1.664)*0.079*((l**2)*g*(particle.ro-1000-ro))/(visc)
+        #? Ws= ((l/d)**-1.664)*0.079*((l**2)*g*(rho))/(visc) #sinking velocity considering density and dimensions change from biofouling
+        particle.ws = Ws
+
+# def Biofilm(particle, fieldset, time):
+#     Nflag = particle.Nflag
+#     Vcell=8.3e-13 #volume Hbac cell
+#     D = particle.diameter*1e2 
+#     L = particle.length*1e2
+#     th2= (Vcell*NN)/(2.5*2*math.pi*(D/2)*L+(D/2)**2) #rough approximation 
+#     D+= 2*th2
+#     L+= 2*th2
+#     ESRt = (((D**2)*3*L/2)**(1/3))/2
+#     Cb = 1.5e6 #aver Bacterial abundance in SoG /cm3 (S.W. Wilhelm et al., 2001)
+#     Cf = 1650 #Estimation Proportional to Hbacteria abundance (Gasol,1994) close to 1640 coastal surface normal average abundance. Fukami 1996 
+#     ###Cf = fieldset.microzooplankton[time, particle.depth, particle.lat, particle.lon]*4733.5 #conversion from mmolNm3 to cell/cm3
+#     Db = 2.33e-5 #Diffusion Bacteria (Kiorbe et al, 2003) cm2/s
+#     Df = 9.8e-5 #Diffusion Het.Nanoflag (Kiorbe et al, 2003)
+#     detb = 2.83e-4 #detaching rate bacteria (Kiorbe et al, 2003)
+#     detf = 6.667e-5 #detaching rate Het.Nanoflag (Kiorbe et al, 2003)
+#     pp = fieldset.PPDIATNO3[time, particle.depth, particle.lat, particle.lon]+fieldset.PPPHYNO3[time, particle.depth, particle.lat, particle.lon]
+#     grb = pp*2.65 #conversion from PP to bacterial growth rate considering 20% of PP ends up as BP
+#     fcl = 8.33e-9 #clearence rate nanoflagelates (Kiorbe et al, 2003)
+#     Pf = (fcl/(1+fcl*3.22e-2*(NN))) #flagellate grazing coefficient
+#     af = Pf*1e-2
+#     Betab = Db/(ESRt*100)
+#     Betaf = Df/(ESRt*100)
+#     Ap = 2*math.pi*((D/2)*L+(D/2)**2) #Surface area of particle.
+#     particle.Nbac += (Betab*Cb*Ap + (grb - detb)*NN -Pf*NN*Nflag)*particle.dt
+#     particle.Nflag +=  (Betaf*Cf*Ap + af*NN*Nflag - detf*Nflag)*particle.dt
+#     if particle.Nbac < 0:
+#         particle.Nbac = 0
+#     if particle.Nflag < 0:
+#         particle.Nflag = 0            
+        
+def Advection(particle, fieldset, time):
+    if particle.beached == 0: #Check particle is in the water column
+        particle.tau += particle.dt
+        bath = fieldset.Bathymetry[time, particle.depth, particle.lat, particle.lon]
+        if particle.tau > particle.dtmax:
+            particle.delete()
+        (u1, v1, w1) = fieldset.UVW[time, particle.depth, particle.lat, particle.lon]
+        lon1 = particle.lon + u1*.5*particle.dt
+        lat1 = particle.lat + v1*.5*particle.dt
+        if copysign(1,particle.ws) != copysign(1,w1):
+            w1 =+ particle.ws
+        else:
+            w1 = copysign(1,w1)*max(math.fabs(w1),math.fabs(particle.ws))
+        dep1 = particle.depth + w1*.5*particle.dt
+        (u2, v2, w2) = fieldset.UVW[time + .5 * particle.dt, dep1, lat1, lon1]
+        lon2 = particle.lon + u2*.5*particle.dt
+        lat2 = particle.lat + v2*.5*particle.dt
+        if copysign(1,particle.ws) != copysign(1,w2):
+            w2 =+ particle.ws
+        else:
+            w2 = copysign(1,w2)*max(math.fabs(w2),math.fabs(particle.ws))
+        dep2 = particle.depth + w2*.5*particle.dt
+        (u3, v3, w3) = fieldset.UVW[time + .5 * particle.dt, dep2, lat2, lon2]
+        lon3 = particle.lon + u3*particle.dt
+        lat3 = particle.lat + v3*particle.dt
+        if copysign(1,particle.ws) != copysign(1,w3):
+            w3 =+ particle.ws
+        else:
+            w3 = copysign(1,w3)*max(math.fabs(w3),math.fabs(particle.ws))
+        dep3 = particle.depth + w3*particle.dt
+        (u4, v4, w4) = fieldset.UVW[time + particle.dt, dep3, lat3, lon3]
+        if copysign(1,particle.ws) != copysign(1,w4):
+            w4 =+ particle.ws
+        else:
+            w4 = copysign(1,w4)*max(math.fabs(w4),math.fabs(particle.ws))
+        particle.lon += (u1 + 2*u2 + 2*u3 + u4) / 6. * particle.dt
+        particle.lat += (v1 + 2*v2 + 2*v3 + v4) / 6. * particle.dt
+        particle.depth += (w1 + 2*w2 + 2*w3 + w4) / 6. * particle.dt
+        if particle.depth > bath:
+            particle.beached = 3 #Trap particle in sediment (sticky bottom)
+        elif particle.depth < 0.5: 
+            particle.depth = 0.5
+
+
+# def Stokes_drift(particle, fieldset, time):
+#     """Stokes drift"""
+#     if particle.beached == 0:
+#         lat = particle.lat
+#         if lat > 48 and lat < 51: #Check that particle is inside WW3 data field
+#             deg2met_st = 111319.5
+#             latT_st = 0.6495 #cos(particle.lat*(math.pi/180))
+#             (us0, vs0, wl) = fieldset.stokes[time, particle.depth, particle.lat, particle.lon]
+#             k = (2*math.pi)/wl
+#             us = (us0*exp(-math.fabs(2*k*particle.depth)))/(deg2met_st*latT_st)
+#             vs = (vs0*exp(-math.fabs(2*k*particle.depth)))/deg2met_st
+#             particle.lon += us * particle.dt 
+#             particle.lat += vs * particle.dt
+
+def Stokes_driftRK3(particle, fieldset, time):
+    """Stokes drift solved with 3rd order RungeKutta"""
+    if particle.beached == 0:
+        lat = particle.lat
+        if lat > 48 and lat < 51: #Check that particle is inside WW3 data field
+            deg2met = 111319.5
+            latT = 0.6495 #cos(particle.lat*(math.pi/180))
+            (us0, vs0, wl) = fieldset.stokes[time, particle.depth, particle.lat, particle.lon]
+            k = (2*math.pi)/wl
+            us1 = (us0*exp(-math.fabs(2*k*particle.depth)))/(deg2met*latT)
+            vs1 = (vs0*exp(-math.fabs(2*k*particle.depth)))/deg2met
+            lon1s = particle.lon + us1 * (1/3) *particle.dt 
+            lat1s = particle.lat + vs1 * (1/3) * particle.dt
+            (us0, vs0, wl) = fieldset.stokes[time + (1/3) * particle.dt, particle.depth, lat1s, lon1s]
+            k = (2*math.pi)/wl
+            us2 = (us0*exp(-math.fabs(2*k*particle.depth)))/(deg2met*latT)
+            vs2 = (vs0*exp(-math.fabs(2*k*particle.depth)))/deg2met
+            lon2s = particle.lon + us2 * .5 *particle.dt 
+            lat2s = particle.lat + vs2 * .5 * particle.dt
+            (us0, vs0, wl) = fieldset.stokes[time + .5 * particle.dt, particle.depth, lat2s, lon2s]
+            k = (2*math.pi)/wl
+            us3 = (us0*exp(-math.fabs(2*k*particle.depth)))/(deg2met*latT)
+            vs3 = (vs0*exp(-math.fabs(2*k*particle.depth)))/deg2met
+            particle.lon +=  us3 * .5 *particle.dt 
+            particle.lat +=  vs3 * .5 * particle.dt
+
+
+def turb_mix(particle,fieldset,time):
+    """Vertical mixing and applying buoyancy"""
+    if particle.beached==0:
+        if particle.depth + 0.5 > bath: #Only calculate gradient of diffusion for particles deeper than 0.6 otherwise OP will check for particles outside the domain and remove it.
+            Kzdz = 0
+        else: 
+            Kzdz = 2*(fieldset.vert_eddy_diff[time, particle.depth+0.5, particle.lat, particle.lon]-fieldset.vert_eddy_diff[time, particle.depth, particle.lat, particle.lon]) #forward difference 
+        dgrad = Kzdz*particle.dt
+        if particle.depth+0.5*dgrad > 0.5:
+            Kz = fieldset.vert_eddy_diff[time, particle.depth+0.5*dgrad, particle.lat, particle.lon] #Vertical diffusivity SSC  #
+        else:
+            Kz = fieldset.vert_eddy_diff[time, 0.5, particle.lat, particle.lon] #Vertical diffusivity SSC  #
+
+        Rr = ParcelsRandom.uniform(-1, 1)
+        d_random = sqrt(3*2*Kz*particle.dt) * Rr
+        dzs = dgrad + d_random
+    ####Boundary conditions 
+        Dlayer = 0.5*sqrt(Kz*particle.dt) #mixing layer dependant on Kz
+        #apply turbulent mixing.
+        if dzs + particle.depth > bath: #randomly in the water colum01n
+            particle.depth = bath - Dlayer * ParcelsRandom.uniform(0, 1)
+        elif particle.depth + dzs < 0:
+            particle.depth = Dlayer * ParcelsRandom.uniform(0, 1) #Well mixed boundary layer
+        else:
+            particle.depth += dzs #apply mixing
+
+
+def Beaching(particle, fieldset, time):
+    """Horizontal mixing to impose beaching for particles reaching coast"""  
+    if particle.beached == 0:
+        kh = particle.Kh
+        Rrx = ParcelsRandom.uniform(-1, 1)
+        Rry = ParcelsRandom.uniform(-1, 1)
+        d_x = sqrt(3*2*kh*particle.dt) * Rrx
+        d_y = sqrt(3*2*kh*particle.dt) * Rry   
+        #deg2met = 111319.5
+        #latT = 0.6495
+        d_randomx = particle.lon + d_x/(deg2met*latT)
+        d_randomy = particle.lat + d_y/deg2met
+        Sbh = fieldset.vosaline[time, 1, d_randomy, d_randomx] #Check if particles reach coast (Salinity = 0)
+        if particle.lat < 49.237 and particle.lon > -123.196 and particle.lat > 49.074:
+            pass #Dont let particles beach inside the fraser river
+        elif Sbh == 0:
+            particle.beached = 1
+        else:
+            particle.lat = d_randomy
+            particle.lon = d_randomx
+
+def Unbeaching(particle, fieldset, time):
+    '''Resuspension prob'''  
+    if particle.beached == 1: 
+        particle.tau += particle.dt
+        if particle.tau > particle.dtmax:
+            particle.delete()       
+        Ub = particle.Ub*86400  #timescale unbeaching in seconds
+        Pr = 1 - exp(-particle.dt/Ub)
+        if ParcelsRandom.uniform(0,1)<Pr:
+            particle.beached = 0
+    elif particle.beached == 3:
+        particle.tau += particle.dt
+
+def DeleteParticle(particle, fieldset, time):
+    """Delete particle from OceanParcels simulation to avoid run failure"""
+    print(f'Particle {particle.id} lost !! [{particle.time}, {particle.depth}, {particle.lat}, {particle.lon}]')
+    particle.delete()
